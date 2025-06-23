049a2550072869f2de9dec481ea0450aae134df8
chocozzangzzang <jangjh_05@naver.com>
2025-06-23 14:30:18 +0900

    video upload

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,10 @@
 import { relations } from "drizzle-orm";
 import { integer, pgEnum, pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
+import {
+    createInsertSchema,
+    createSelectSchema,
+    createUpdateSchema,
+} from "drizzle-zod";
 
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
@@ -35,13 +40,13 @@ export const videoVisibility = pgEnum("video_visibility", [
 export const videos = pgTable("videos", {
     id: uuid("id").primaryKey().defaultRandom(),
     title: text("title").notNull(),
-    descrption: text("description"),
+    description: text("description"),
     muxStatus: text("mux_status"),
     muxAssetId: text("mux_asset_id").unique(),
     muxUploadId: text("mux_upload_id").unique(),
     muxPlaybackId: text("mux_playback_id").unique(),
     muxTrackId: text("mux_track_id").unique(),
-    muxTrackStatus: text("mux_track_status").unique(),
+    muxTrackStatus: text("mux_track_status"),
     thumbnailUrl: text("thumbnail_url"),
     previewUrl: text("preview_url"),
     duration: integer("duration").default(0).notNull(),
@@ -55,6 +60,10 @@ export const videos = pgTable("videos", {
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+export const videoInsertSchema = createInsertSchema(videos);
+export const videoUpdateSchema = createUpdateSchema(videos);
+export const videoSelectSchema = createSelectSchema(videos);
 
 // join 기능이 제공되는 SQL이라면 relation을 정의하지 않아도 됨 //
 export const videoRelations = relations(videos, ({ one }) => ({
--- src/modules/studio/ui/section/video-section.tsx
+++ src/modules/studio/ui/section/video-section.tsx
@@ -19,14 +19,72 @@ import Link from "next/link";
 import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
 import { snakeCaseToTitle } from "@/lib/utils";
 import { Globe2Icon, LockIcon } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const VideosSection = () => {
     return (
-        <Suspense fallback={<p>....Loading......</p>}>
+        <Suspense fallback={<VideoSectionSkeleton />}>
             <ErrorBoundary fallback={<p>....Error....</p>}>
                 <VideosSectionSuspense />
             </ErrorBoundary>
         </Suspense>
+    )
+}
+
+const VideoSectionSkeleton = () => {
+    return (
+        <>
+            <div className="border-y">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="pl-6 w-[510px]">Video</TableHead>
+                            <TableHead>Visibility</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Date</TableHead>
+                            <TableHead>Views</TableHead>
+                            <TableHead>Comments</TableHead>
+                            <TableHead>Likes</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody className="pl-6">
+                        {
+                            Array.from({ length : 5}).map((_, index) => (
+                                <TableRow key={index}>
+                                    <TableCell className="pl-6">
+                                        <div className="flex items-center gap-4">
+                                            <Skeleton className="h-20 w-36"/>
+                                            <div className="flex flex-col gap-2">
+                                                <Skeleton className="h-4 w-[100px]" />
+                                                <Skeleton className="h-3 w-[150px]" />
+                                            </div>
+                                        </div>
+                                    </TableCell>
+                                    <TableCell>
+                                        <Skeleton className="h-4 w-20" />
+                                    </TableCell>
+                                    <TableCell>
+                                        <Skeleton className="h-4 w-16" />
+                                    </TableCell>
+                                    <TableCell>
+                                        <Skeleton className="h-4 w-24" />
+                                    </TableCell>
+                                    <TableCell className="text-right">
+                                        <Skeleton className="h-4 w-12 ml-auto" />
+                                    </TableCell>
+                                    <TableCell className="text-right">
+                                        <Skeleton className="h-4 w-12 ml-auto" />
+                                    </TableCell>
+                                    <TableCell className="text-right pr-6">
+                                        <Skeleton className="h-4 w-12 ml-auto" />
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+            </div>
+        </>
     )
 }
 
@@ -57,7 +115,7 @@ const VideosSectionSuspense = () => {
                         {videos.pages.flatMap((page) => page.items).map((video) => (
                             <Link href={`/studio/videos/${video.id}`} key={video.id} legacyBehavior>
                                 <TableRow className="cursor-pointer">
-                                    <TableCell>
+                                    <TableCell className="pl-6">
                                         <div className="flex items-center gap-4">
                                             <div className="relative aspect-video w-36 shrink-0">
                                                 <VideoThumbnail 
@@ -70,7 +128,7 @@ const VideosSectionSuspense = () => {
                                             <div className="flex flex-col overflow-hidden gap-y-1">
                                                 <span className="text-sm line-clamp-1">{video.title}</span>
                                                 <span className="text-sm text-muted-foreground line-clamp-1">
-                                                    {video.descrption || "No Description"}
+                                                    {video.description || "No Description"}
                                                 </span>
                                             </div>
                                         </div>
@@ -95,13 +153,13 @@ const VideosSectionSuspense = () => {
                                             {format(new Date(video.createdAt), "d MMM yyyy") }
                                         </div>
                                     </TableCell>
-                                    <TableCell>
+                                    <TableCell className="text-right text-sm">
                                         view
                                     </TableCell>
-                                    <TableCell>
+                                    <TableCell className="text-right text-sm">
                                         comments
                                     </TableCell>
-                                    <TableCell>
+                                    <TableCell className="text-right text-sm pr-6">
                                         likes
                                     </TableCell>
                                 </TableRow>
