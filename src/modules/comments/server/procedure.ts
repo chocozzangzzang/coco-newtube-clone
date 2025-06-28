c805fa21777a4b617fafc7ad3891163585416414
chocozzangzzang <jangjh_05@naver.com>
2025-06-28 18:30:00 +0900

    reply for comments

--- src/modules/comments/server/procedure.ts
+++ src/modules/comments/server/procedure.ts
@@ -2,22 +2,37 @@ import { db } from "@/db";
 import { commentReactions, comments, users } from "@/db/schema";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
-import { and, count, desc, eq, getTableColumns, inArray, lt, or } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, inArray, isNotNull, isNull, lt, or } from "drizzle-orm";
 import { z } from "zod";
 
 export const commentsRouter = createTRPCRouter({
     create: protectedProcedure
         .input(z.object({
+            parentId: z.string().uuid().nullish(),
             videoId: z.string().uuid(),
             value: z.string(),
         }))
         .mutation(async ({ input, ctx }) => {
-            const { videoId, value } = input;
+            const { parentId, videoId, value } = input;
             const { id : userId } = ctx.user;
+
+            // 1 level reply //
+            const [ existingComment ] = await db
+                .select()
+                .from(comments)
+                .where(inArray(comments.id, parentId ? [parentId] : []));
+            
+            if(!existingComment && parentId) {
+                throw new TRPCError({ code : "NOT_FOUND"});
+            }
+
+            if(existingComment?.parentId && parentId) {
+                throw new TRPCError({ code : "BAD_REQUEST"});
+            }
 
             const [ createdComment ] = await db
                 .insert(comments)
-                .values({ userId, videoId, value })
+                .values({ userId, videoId, parentId, value })
                 .returning();
 
             return createdComment;
@@ -48,6 +63,7 @@ export const commentsRouter = createTRPCRouter({
         .input(
             z.object({
                 videoId: z.string().uuid(),
+                parentId: z.string().uuid().nullish(),
                 cursor: z.object({
                     id: z.string().uuid(),
                     updatedAt: z.date(),
@@ -55,7 +71,7 @@ export const commentsRouter = createTRPCRouter({
                 limit: z.number().min(1).max(100),
             })
         ).query(async({ input, ctx }) => {
-            const { videoId, cursor, limit } = input;
+            const { videoId, parentId, cursor, limit } = input;
             const { clerkUserId } = ctx;
 
             let userId;
@@ -68,7 +84,6 @@ export const commentsRouter = createTRPCRouter({
             if(user) {
                 userId = user.id;
             }
-
             
             const viewerReactions = db.$with("viewer_reactions").as(
                 db
@@ -79,6 +94,17 @@ export const commentsRouter = createTRPCRouter({
                     .from(commentReactions)
                     .where(inArray(commentReactions.userId, userId ? [userId] : []))
             );
+
+            const replies = db.$with("replies").as(
+                db
+                    .select({
+                        parentId: comments.parentId,
+                        count: count(comments.id).as("count"),
+                    })
+                    .from(comments)
+                    .where(isNotNull(comments.parentId))
+                    .groupBy(comments.parentId)
+            )
             
             const [ totalData, data ] = await Promise.all([
                 db
@@ -86,14 +112,17 @@ export const commentsRouter = createTRPCRouter({
                         count: count(),
                     })
                     .from(comments)
-                    .where(eq(comments.videoId, videoId)),
+                    .where(and(
+                        eq(comments.videoId, videoId),
+                        isNull(comments.parentId))),
 
                 db
-                    .with(viewerReactions)
+                    .with(viewerReactions, replies)
                     .select({
                         ...getTableColumns(comments),
                         user : users,
                         viewerReaction: viewerReactions.type,
+                        replyCount: replies.count,
                         likeCount: db.$count(
                             commentReactions,
                             and(
@@ -112,6 +141,9 @@ export const commentsRouter = createTRPCRouter({
                     .from(comments)
                     .where(and(
                         eq(comments.videoId, videoId),
+                        parentId
+                            ? eq(comments.parentId, parentId)
+                            : isNull(comments.parentId),
                         cursor
                             ? or(
                                 lt(comments.updatedAt, cursor.updatedAt),
@@ -123,6 +155,7 @@ export const commentsRouter = createTRPCRouter({
                     ))
                     .innerJoin(users, eq(comments.userId, users.id))
                     .leftJoin(viewerReactions, eq(viewerReactions.commentId, comments.id))
+                    .leftJoin(replies, eq(comments.id, replies.parentId))
                     .orderBy(desc(comments.updatedAt), desc(comments.id))
                     .limit(limit + 1)
             ]);
--- src/modules/comments/ui/components/comment-form.tsx
+++ src/modules/comments/ui/components/comment-form.tsx
@@ -12,12 +12,18 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 interface CommentFormProps {
     videoId: string;
+    parentId?: string;
     onSuccess?: () => void;
+    onCancel?: () => void;
+    variant?: "comment" | "reply";
 };
 
 export const CommentForm = ({
     videoId,
+    parentId,
     onSuccess,
+    onCancel,
+    variant = "comment"
 } : CommentFormProps) => {
 
     const { user } = useUser();
@@ -26,7 +32,8 @@ export const CommentForm = ({
     const utils = trpc.useUtils();
     const create = trpc.comments.create.useMutation({
         onSuccess: () => {
-            utils.comments.getMany.invalidate({ videoId: videoId });
+            utils.comments.getMany.invalidate({ videoId });
+            utils.comments.getMany.invalidate({ videoId, parentId });
             form.reset();
             toast.success("Comment Added");
             onSuccess?.();
@@ -43,6 +50,7 @@ export const CommentForm = ({
     const form = useForm<z.infer<typeof commentInsertSchema>>({
         resolver: zodResolver(commentInsertSchema.omit({ userId : true})),
         defaultValues: {
+            parentId: parentId,
             videoId,
             value: "",
         }
@@ -53,6 +61,11 @@ export const CommentForm = ({
             ...values,
             videoId,
         });
+    }
+
+    const handleCancel = () => {
+        form.reset();
+        onCancel?.();
     }
 
     return (
@@ -74,7 +87,11 @@ export const CommentForm = ({
                                 <FormControl>
                                     <Textarea
                                         {...field}
-                                        placeholder="Add a comment...."
+                                        placeholder={
+                                            variant === "comment"?
+                                            "Add a comment...." :
+                                            "Reply to this comment"
+                                        }
                                         className="resize-none bg-transparent overflow-hidden min-h-0"
                                     />
                                 </FormControl>
@@ -84,12 +101,19 @@ export const CommentForm = ({
                     />
                     
                     <div className="justify-end gap-2 mt-2 flex">
+                        {
+                            onCancel && (
+                                <Button variant="ghost"  type="button" onClick={handleCancel}>
+                                    cancel
+                                </Button>
+                            )
+                        }
                         <Button
                             disabled={create.isPending}
                             type="submit"
                             size="sm"
                         >
-                            Comment
+                            {variant === "comment"? "Comment" : "Reply"}
                         </Button>
                     </div>
                 </div>
